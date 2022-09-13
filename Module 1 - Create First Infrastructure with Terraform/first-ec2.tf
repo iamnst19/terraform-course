fd313bad15d91d517262eede9995b9a993bd1988
iamnst19 <importpythin@gmail.com>
2022-09-13 19:54:10 +0530

    changes

--- Module 1 - Create First Infrastructure with Terraform/first-ec2.tf
+++ Module 1 - Create First Infrastructure with Terraform/first-ec2.tf
@@ -1,10 +1,10 @@
 terraform {
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "~> 3.0"
-      }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.30.0"
     }
+  }
 }
 
 provider "aws" {
@@ -13,10 +13,14 @@ provider "aws" {
   secret_key = "jc+T54UY69Ma69oYGRjetjyUP4Lwnt/jebOhedK9"
 }
 
-resource "aws_instance" "myec2" {
-   ami = "ami-06489866022e12a14"
-   instance_type = "t2.micro"
-}
+# resource "aws_instance" "myec2" {
+#    ami = "ami-06489866022e12a14"
+#    instance_type = "t2.micro"
+# }
+
+# output "eip" {
+#   value = aws_instance.myec2.arn
+# }
 
 
 # provider "github" {
---
+++ Module 1 - Create First Infrastructure with Terraform/terraform-new/ec2.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "2.30.0"
+    }
+  }
+}
+
+# provider "aws" {
+#   region     = "ap-south-1"
+#   access_key = "AKIA33CHTBYXMOCUEFFC"
+#   secret_key = "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"
+# }
